060fe2b6ccbf315d7be69d7ba24185d49d72346c
Trushang-Patel <22it116@charusat.edu.in>
2025-02-27 19:59:37 +0530

    1. Somke  Test

---
+++ contracts/TrushangToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.0;
+
+contract TrushangToken{
+    //Constructer
+    //Set the total number of tokens
+    //Read the total number of tokens
+    uint256 public totalSupply;
+    constructor(){
+        totalSupply = 1000000;
+    }
+    
+}
---
+++ migrations/1_deploy_contract.js
@@ -0,0 +1,5 @@
+var TrushangToken = artifacts.require('./TrushangToken.sol');
+
+module.exports = function(deployer) {
+    deployer.deploy(TrushangToken);
+};
