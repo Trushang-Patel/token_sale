379f6fa8a1131f6600dfd55d06261103f4aa41a2
Trushang-Patel <22it116@charusat.edu.in>
2025-03-02 09:48:36 +0530

    7.  Purchasing ERC-20 Tokens

--- contracts/TrushangTokenSale.sol
+++ contracts/TrushangTokenSale.sol
@@ -7,6 +7,9 @@ contract TrushangTokenSale {
     address admin;
     TrushangToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+    
+    event Sell(address _buyer, uint256 _amount);
 
     constructor(TrushangToken _tokenContract,uint256 _tokenPrice) {
         //Assign an admin
@@ -15,6 +18,28 @@ contract TrushangTokenSale {
         tokenContract = _tokenContract;
         //Token Price
         tokenPrice = _tokenPrice;
-
     }
+
+    //Multiply
+    function multiply(uint x, uint y) internal pure returns (uint z){
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    //Buy Tokens
+    function buyTokens(uint256 _numberOfTokens) public payable{
+        //Require that value is equal to tokens
+        require(msg.value == multiply(_numberOfTokens,tokenPrice));
+
+        //Require that the contract has enough tokens
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+
+        //keep that tokens transfer is successful
+         require(tokenContract.transfer(msg.sender, _numberOfTokens));
+        
+        //Keep track of tokenSold
+        tokensSold += _numberOfTokens;
+        //Trigger sell event
+        emit Sell(msg.sender, _numberOfTokens);
+    }
+
 }
--- test/TrushangTokenSale.js
+++ test/TrushangTokenSale.js
@@ -1,8 +1,14 @@
 var TrushangTokenSale = artifacts.require('./TrushangTokenSale.sol');
+var TrushangToken = artifacts.require('./TrushangToken.sol');
 
 contract('TrushangTokenSale', function(accounts) {
     var tokenSaleInstance;
+    var tokenInstance;
+    var admin = accounts[0];
+    var buyer = accounts[1];
     var tokenPrice = 1000000000000000; // in wei
+    var tokensAvailable = 750000;
+    var numberOfTokens;
 
     it('initializes the contract with the correct values', function() {
         return TrushangTokenSale.deployed().then(function(instance) {
@@ -18,4 +24,42 @@ contract('TrushangTokenSale', function(accounts) {
             assert.equal(price,tokenPrice, 'token price is correct');
         });
     });
+
+    it('facilitates token buying', function() {
+        return TrushangToken.deployed().then(function(instance) {
+            //Grab token instance first
+            tokenInstance = instance;
+            return TrushangTokenSale.deployed()
+        }).then(function(instance) {
+            //Then grab token sale instance;
+            tokenSaleInstance = instance;
+            // Provision 75% of all tokens to the token sale
+            return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, { from: admin })
+        }).then(function(receipt) {
+            numberOfTokens = 10;
+            return tokenSaleInstance.buyTokens(numberOfTokens, { from:buyer, value: numberOfTokens * tokenPrice });
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+            assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
+            assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+            return tokenSaleInstance.tokensSold();
+        }).then(function(amount) {
+            assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
+            return tokenInstance.balanceOf(buyer);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), numberOfTokens);
+            return tokenInstance.balanceOf(tokenSaleInstance.address);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+            // Try to buy tokens different from the ether value
+            return tokenSaleInstance.buyTokens(numberOfTokens, { from:buyer, value: 1 });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+            return tokenSaleInstance.buyTokens(800000, { from:buyer, value: numberOfTokens * tokenPrice });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot purchase more tokens than available');
+        });
+    });
+
 });
